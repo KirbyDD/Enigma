b2591b3c2f61502c684eaa867e296a4527861b9a
DeMarcus Kirby <tkd024@gmail.com>
2019-11-06 00:10:03 -0700

    Add decrypt class testing suite

---
+++ test/decrypt_test.rb
@@ -0,0 +1,22 @@
+require "./test/test_helper.rb"
+
+class EncryptTest < Minitest::Test 
+  def setup
+    @decrypt = Decrypt.new("keder ohulw", "02715", "040895")
+  end
+
+  def test_it_exist
+    assert_instance_of Decrypt, @decrypt
+  end
+
+  def test_it_has_attributes
+    assert_equal "keder ohulw",  @decrypt.msg
+    assert_equal "02715",  @decrypt.key
+    assert_equal "040895",  @decrypt.date
+  end
+
+  def test_decrypt_method
+    expected = {encryption: "hello world", key: "02715", date: "040895"}
+    assert_equal expected, @decrypt.decrypt_msg
+  end
+end
