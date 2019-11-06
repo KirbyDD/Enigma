583f5bdddda034a711355ec4f1622c6171725e47
DeMarcus Kirby <tkd024@gmail.com>
2019-11-06 01:17:20 -0700

    Add test for optional arguments

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -18,4 +18,17 @@ class EnigmaTest < Minitest::Test
     expected = {decryption: "hello world!", key: "02715", date: "040895"}
     assert_equal expected, @enigma.decrypt("keder ohulw!", "02715", "040895")
   end
+
+  def test_encrypt_method_no_date
+    expected = {encryption: "lfhasasdvm !", key: "02715", date: "061119"}
+    assert_equal expected, @enigma.encrypt("hello world!", "02715")
+  end
+
+  def test_encrypt_method_no_date_or_key
+    expected = {encryption: "lfhasasdvm !", key: "02715", date: "061119"}
+    assert_instance_of Hash, @enigma.encrypt("hello world!")
+    assert_equal true, @enigma.encrypt("hello world!").keys.include?(:encryption)
+    assert_equal true, @enigma.encrypt("hello world!").keys.include?(:key)
+    assert_equal true, @enigma.encrypt("hello world!").keys.include?(:date)
+  end
 end
