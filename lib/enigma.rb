e8b37a11f30077aaecdfd9467b99c52abd268e73
DeMarcus Kirby <tkd024@gmail.com>
2019-11-06 01:07:01 -0700

    Add enigma-test suite

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,11 +4,15 @@ require_relative './encryption_module.rb'
 class Enigma
   include Encryption
 
-  def encrypt(msg, key, date = today)
+  def encrypt(msg, key = create_key, date = today)
     result = Encrypt.new(msg, key, date)
     result.create_encryption
   end
 
+  def decrypt(msg, key, date = today)
+    result = Decrypt.new(msg, key, date)
+    result.decrypt_msg
+  end
 end
 
 # enigma = Enigma.new
